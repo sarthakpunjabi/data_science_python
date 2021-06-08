2cf732f93bc53d3d98efcb336c6021a3561b2b77
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2021-06-08 12:31:10 +0530

    Adding data.csv and making of preprocess.ipynb in which there is all the terms for future machine learning  models and train_test split

--- US_game/main.py
+++ US_game/main.py
@@ -4,10 +4,29 @@ import pandas as pd
 
 cursor = Turtle()
 screen = Screen()
-screen.setup(width=600,height=800)
-screen.tit
+screen.setup(width=700,height=500)
+screen.title("US states recognization ")
+screen.bgpic("blank_states_img.gif")
+
+
+# def get_mouse_on_click(x,y):
+#     print(x,y)
+
+# screen.onscreenclick(get_mouse_on_click)
+# screen.mainloop()
 
 states = pd.read_csv("50_states.csv")
+answer = screen.textinput(title="Guess the state" , prompt="what's the another state game ").capitalize()
+
+try:
+    ans = states[states.state == answer]
+    
+    cursor.write(f'{ans.state}',font=("Verdana",15, "normal"))
+    
+except:
+    print("something went wrong ")
+
+
+
+
 screen.exitonclick()
-
-
